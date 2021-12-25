a874ceb57a479cc67f58a821602ed50ebefc47f6
aecio prado <aecioprado@gmail.com>
2021-12-25 15:37:17 -0300

    setup

---
+++ src/main/java/colmeia/api/colmeiaapi/ColmeiaApiApplication.java
@@ -0,0 +1,13 @@
+package colmeia.api.colmeiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColmeiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColmeiaApiApplication.class, args);
+	}
+
+}
