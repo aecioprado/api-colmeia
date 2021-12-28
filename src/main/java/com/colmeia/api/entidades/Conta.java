4189becad5b8d064def27a680f4493783d5f1747
aecio prado <aecioprado@gmail.com>
2021-12-28 05:36:45 -0300

    Dominio da Conta: incluidos Conta e IContaRepositorio

--- src/main/java/com/colmeia/api/entidades/Conta.java
+++ src/main/java/com/colmeia/api/entidades/Conta.java
@@ -1,7 +1,10 @@
 package com.colmeia.api.entidades;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,20 +23,26 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "tb_conta")
+@Table(name = "conta")
 public class Conta implements Serializable{
 	
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@ManyToOne
-    @JoinColumn(name="cliente_id", nullable=false)
+	//FK
+	@JsonIgnore
+	@ManyToOne//(cascade=CascadeType.ALL)
+	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
+	
+	@Column(name = "saldo_inicial")
+	private BigDecimal saldoInicial;
+	
+	@Column(name = "descricao")
+	private String descricao;
 	
 }
--- src/main/java/com/colmeia/api/repositorios/IContaRepositorio.java
+++ src/main/java/com/colmeia/api/repositorios/IContaRepositorio.java
@@ -1,9 +1,16 @@
 package com.colmeia.api.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.colmeia.api.entidades.Conta;
 
 public interface IContaRepositorio extends JpaRepository<Conta, Long>{
+	
+	@Query(value="select * from conta c where c.cliente_id= :id", nativeQuery=true)
+	public List<Conta> listarContasPorClienteId(Long id);
+	
 
 }
